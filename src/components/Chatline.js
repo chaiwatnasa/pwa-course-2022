82011b4f3a6ea397a73e2f1ebff9942577bc2b8c
chaiwatnasa <chaiwat.nasa@bumail.net>
2022-03-19 16:23:39 +0700

    1630700993

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -18,18 +18,27 @@ function Chatline(props) {
       const [, name] = text.split(" ");
       fetch("https://api.agify.io/?name=" + name)
         .then(res => res.json())
-        .then(data => setElement(data.age));
+        .then(data => setElement(data.name + " ; " +data.age));
     }
     else if (text.startsWith("/gender")) {
       const [, name] = text.split(" ");
       fetch("https://api.genderize.io/?name=" + name)
         .then(res => res.json())
-        .then(data => setElement(data.gender));
+        .then(data => setElement(data.gender || "ไม่เจอ"));
     }
-    else if (text.startsWith("/randomfact")) {
+    else if (text.startsWith("/radomfact")) {
       // ex10-01 here: Implement randomfact using fetch & promise
       // https://uselessfacts.jsph.pl/random.json?language=en
-
+      const [, name] = text.split(" ");
+      fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+        .then(res => res.json())
+        .then(data => setElement(data.text));
+    }
+    else if (text.startsWith("/nationalize")) {
+      const [, name] = text.split(" ");
+      fetch("https://api.nationalize.io/?name=" + name)
+        .then(res => res.json())
+        .then(data => setElement(data.contry || "ไม่เจอ"));
     }
   };
 
